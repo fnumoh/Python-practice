63e2658816c2c121b2c640caef0c38c3f0884f6f
fnumoh <115008054+fnumoh@users.noreply.github.com>
2022-10-04 09:25:52 -0400

    file1

---
+++ 1.py
@@ -0,0 +1,20 @@
+A = int(input("Please enter the student 1 mark:  "))
+if A < 100 and A > 90:
+    print( "A Grade")
+else:
+    print("F Grade")
+B = int(input("Please enter the student 2 mark:  "))
+if B < 89 and B > 70:
+    print("B Grade")
+else:
+    print("F Grade")
+C = int(input("Please enter the student 3 mark:  "))
+if C < 69 and C > 60:
+    print("C Grade")
+else:
+    print("F Grade")
+D = int(input("Please enter the student 4 mark:  "))
+if D < 59 and D > 50:
+    print("D Grade")
+else:
+    print("F Grade")
