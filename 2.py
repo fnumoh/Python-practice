bdc4838c64ffe41f9cfe7a54277e072fece9a43e
fnumoh <115008054+fnumoh@users.noreply.github.com>
2022-10-04 09:26:47 -0400

    file2

---
+++ 2.py
@@ -0,0 +1,11 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
+it_companies.add('Twitter')
+print(it_companies)
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+print(len(it_companies))
+a={'Google','TCSL','Upenn','AsianPaints'}
+it_companies.update(a)
+print(it_companies)
